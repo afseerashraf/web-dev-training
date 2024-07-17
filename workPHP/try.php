be04d654a08440e3125bfcf0748bde9479dc5115
AFSEER <afseerashraf007@gmail.com>
2024-07-18 00:24:26 +0530

    php building and user defind function

--- workPHP/try.php
+++ workPHP/try.php
@@ -1,47 +1,72 @@
+<?php
+
+function findValue($array,$value){
+    for($i = 0; $i < count($array); $i++){
+       if($array[$i] == $value){
+        return $array[$i];
+       }
+    }
+    return "No Item";
+}
+
+$fruits = ['apple', 'banana', 'orange'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php</title>
     <style>
-        .pattern{
-            margin-left: 100px;
-        }
-        .pattern_2{
-            margin-top:-94px;
-            margin-left: 102px;
 
-        }
+       .apple{
+        margin: auto;
+        height:100px;
+        width:100px;
+        background-color: red;
+       }
+       .banana{
+        margin: auto;
+        height:100px;
+        width:100px;
+        background-color: yellow;
+        color:black;
+       }
+       .orange{
+        margin: auto;
+        height:100px;
+        width:100px;
+        background-color: orange;
+        color:black;
+       }
+       .date{
+        font-size: 2em;
+        animation: fadeIn 1s ease-in-out infinite alternate;
+       }
+       
     </style>
 </head>
 <body>
-<h1>pattern</h1>
-<div class="pattern">
-<?php
-   
-   for($i = 1; $i <= 5; $i++){
-    for($j = 1; $j <= $i; $j++){
-        echo "AFz";
-    }
-   
-    echo "<br>";
-   }
-   
-?>
+ <header>
+ <h1>Fruit shop</h1>
+
+ </header>
+
+ <marquee behavior="slow" direction="right"></marquee><p class="date">Itams add date <?php echo date('Y-M-D H:i:s') ?></p> </marquee>
+
+<div class="apple">
+<h3><?php echo findValue($fruits, 'apple') ?> </h3>
 </div>
-<div class="pattern_2">
-<?php
-   
-   for($i = 1; $i <= 5; $i++){
-    for($j = 1; $j <= $i; $j++){
-        echo "AFz";
-    }
-   
-    echo "<br>";
-   }
-   
-?>
+
+<div class="banana">
+<h3><?php echo findValue($fruits, 'banana') ?> </h3>
+</div>
+<div class="orange">
+<h3><?php echo findValue($fruits, 'orange') ?> </h3>
+</div>
+
+
+
 </div>
     
 </body>
